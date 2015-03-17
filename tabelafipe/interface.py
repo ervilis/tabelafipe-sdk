4a769c29b641bb108e45186f580d10aa540647cb
Ervilis Souza <ervilisviana@gmail.com>
2015-03-17 00:15:03 -0300

    Adicionando interface de comunicação com os endpoints de acesso aos dados

---
+++ tabelafipe/interface.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+"""Módulo de comunicação com o site oficial da tabela fipe."""
+
+
+class Interface(object):
+
+    """Interface de comunicação com os endpoints de acesso aos dados."""
+
+    pass
