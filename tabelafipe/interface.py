b29f59953efd00e941a924bf065f016ca8c6356a
Ervilis Souza <ervilisviana@gmail.com>
2015-03-17 00:16:45 -0300

    Adicionando especificação dos métodos da interface de comunicação

--- tabelafipe/interface.py
+++ tabelafipe/interface.py
@@ -6,4 +6,38 @@ class Interface(object):
 
     """Interface de comunicação com os endpoints de acesso aos dados."""
 
-    pass
+    tipo_veiculo = None
+
+    tabela_referencia = None
+
+    def __init__(self, tipo_veiculo, tabela_referencia=None):
+        """
+        Inicializamos a interface com o tipo de veículo que será consultado
+        pois essa informação é utilizada em todos os endpoints. Recebemos
+        também opcionalmente a tabela de referência pois esse parâmetro não é
+        necessário em todos os métodos.
+        """
+        self.tipo_veiculo = tipo_veiculo
+        self.tabela_referencia = tabela_referencia
+
+    def get_tabelas_referencia(self):
+        """
+        Obtem as tabelas de referência disponíves desde a criação dos índices.
+        NOTE: Várias tabelas antigas estão com códigos fora de ordem.
+        """
+        pass
+
+    def get_marcas(self):
+        """
+        Obtem as marcas disponíves para o tipo de veículo informado.
+        NOTE: uma mesma marca possúi ID diferente para cada tipo de veículo.
+        """
+        pass
+
+    def get_modelos(self, marca):
+        """Obtem quais são os modelos disponíves da marca informada"""
+        pass
+
+    def get_anos_modelo(self, marca, modelo):
+        """Obtem os anos disponíves para o modelo informado"""
+        pass
